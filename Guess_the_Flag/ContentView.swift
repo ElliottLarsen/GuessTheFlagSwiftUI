af7348176d5b8c04ea6d2ecabf6b3522b55d36f7
Elliott Larsen <elliottlrsn@gmail.com>
2022-08-17 20:17:44 -0600

    Initial commit.

---
+++ Guess_the_Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess_the_Flag
+//
+//  Created by Elliott Larsen on 8/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
