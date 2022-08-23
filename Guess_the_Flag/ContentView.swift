76f6cd48432c8af1c58bfea4e5c5292193090218
Elliott Larsen <elliottlrsn@gmail.com>
2022-08-22 19:11:35 -0600

    Flags and country names shuffle.  Alert message is shown.

--- Guess_the_Flag/ContentView.swift
+++ Guess_the_Flag/ContentView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland",
-                     "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland",
+                     "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
+        
         ZStack {
             
             Color.blue
@@ -27,7 +30,7 @@ struct ContentView: View {
                 }
                 ForEach(0..<3) {number in
                     Button {
-                        
+                    flagTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
@@ -36,9 +39,27 @@ struct ContentView: View {
                 }
 
             }
-
+            .alert(scoreTitle, isPresented:  $showingScore) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                Text("Your score is ???")
+            }
         }
 
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct!"
+        } else {
+            scoreTitle = "Wrong!"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 struct ContentView_Previews: PreviewProvider {
